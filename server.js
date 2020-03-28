613b3876f518e6f08df5b574b545f25aab3a04fe
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-03-27 20:01:52 -0700

    components

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ const posts = require('./routes/api/posts');
 const passport = require('passport');
 const app = express();
 
-
-///Body parser middleware
+//Body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -22,12 +20,11 @@ mongoose
   .then(() => console.log('MongoDb connected!'))
   .catch(err => console.log(err));
 
-  //Passport middleware
+
+//Passport middleware
 app.use(passport.initialize());
-
 //Passport config
-require ('./config/passport')(passport)
-
+require('./config/passport')(passport)
 
 //Let's write our first route
 app.get('/', (req,res) => res.send('Hello!'));
@@ -36,5 +33,5 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = 8020;
+const port = 8021;
 app.listen(port, () => console.log(`Server running on port ${port}`) );
